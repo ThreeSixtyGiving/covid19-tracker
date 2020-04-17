3d106b7b6818e0ddd0c80ec1fa9321f60b205f26
David Kane <david@dkane.net>
2020-04-17 13:31:38 +0100

    update location of files

--- covidtracker/settings.py
+++ covidtracker/settings.py
@@ -6,5 +6,5 @@ DB_URL = os.getenv("DB_URL")
 GOOGLE_ANALYTICS = os.getenv('GOOGLE_ANALYTICS')
 GRANTS_DATA_FILE = os.path.join(os.path.dirname(__file__), 'assets/data/grants_data.json')
 FUNDER_IDS_FILE = os.path.join(os.path.dirname(__file__), 'assets/data/funder_ids.json')
-GRANTS_DATA_URL = 'https://raw.githubusercontent.com/ThreeSixtyGiving/covid19-tracker/master/docs/data/grants_data.json'
-FUNDER_IDS_URL = 'https://raw.githubusercontent.com/ThreeSixtyGiving/covid19-tracker/master/docs/data/funder_ids.json'
+GRANTS_DATA_URL = 'https://raw.githubusercontent.com/ThreeSixtyGiving/covid19-tracker/master/covidtracker/assets/data/grants_data.json'
+FUNDER_IDS_URL = 'https://raw.githubusercontent.com/ThreeSixtyGiving/covid19-tracker/master/covidtracker/assets/data/funder_ids.json'
