016c84ff4ff138fffe022c40bc0cde489b5f5f32
David Kane <david@dkane.net>
2020-06-18 15:14:35 +0100

    improved map

--- covidtracker/components/geomap.py
+++ covidtracker/components/geomap.py
@@ -1,3 +1,5 @@
+import datetime
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -7,6 +9,7 @@ from ..settings import THREESIXTY_COLOURS, MAPBOX_TOKEN
 def geomap(data):
     lats = []
     lngs = []
+    texts = []
     for g in data['grants']:
         if g.get("_geo"):
             lat = g["_geo"][0].get("latitude")
@@ -14,40 +17,46 @@ def geomap(data):
             if lat and lng:
                 lats.append(lat)
                 lngs.append(lng)
+                texts.append("""£{amountAwarded:,.0f} on {awardDate:%d/%m/%Y}<br>from {funder} to {recipient}<br>for {title}""".format(
+                    amountAwarded=g.get("amountAwarded"),
+                    awardDate=datetime.datetime.fromisoformat(g.get("awardDate")[0:10]),
+                    recipient=g["recipientOrganization"][0].get("name"),
+                    funder=g["fundingOrganization"][0].get("name"),
+                    title=g.get("title"),
+                ))
     
     return html.Div(
         className="base-card base-card--teal",
         children=[
             html.Div(className="base-card__content", children=[
-                html.Header(className="base-card__header", children=[
-                    html.H3(className="base-card__heading",
-                            children="Grants by"),
-                ]),
                 dcc.Graph(
                     id='geo-map',
                     figure={
-                        'data': [{
-                            'type': 'scattermapbox',
-                            'lat': lats,
-                            'lon': lngs,
-                        }],
+                        'data': [dict(
+                            type='scattermapbox',
+                            lat=lats,
+                            lon=lngs,
+                            text=texts,
+                            hoverinfo='text',
+                        )],
                         'layout': {
                             'mapbox': dict(
                                 accesstoken=MAPBOX_TOKEN,
                                 bearing=0,
                                 center=dict(
-                                    lat=sum(lats) / len(lats),
-                                    lon=-sum(lngs) / len(lngs)
+                                    lat=(sum(lats) / len(lats)),
+                                    lon=(sum(lngs) / len(lngs)),
                                 ),
                                 pitch=0,
-                                zoom=6
+                                zoom=5,
                             ),
                             'margin': dict(l=0, r=9, t=0, b=0),
+                            'height': 800,
                         }
                     },
                     config={
-                        'displayModeBar': False,
-                        'scrollZoom': False,
+                        # 'displayModeBar': False,
+                        # 'scrollZoom': False,
                     }
                 ),
             ]),
