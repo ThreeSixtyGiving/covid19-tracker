6ed0e8e6f6de602a2fb72bb4029a69bfa6dcf954
David Kane <david@dkane.net>
2020-06-19 14:57:14 +0100

    add local authority summaries

--- covidtracker/layout.py
+++ covidtracker/layout.py
@@ -17,8 +17,8 @@ def layout(data, all_data):
                     html.Div(id="data-cards", children=cards(data)),
                     html.Div(className="spacer-5"),
                     html.Div(className="", children=filters(all_data)),
-                    dcc.Tabs(parent_className='wrapper', className='filters', children=[
-                        dcc.Tab(label='Dashboard', className='', selected_className='', children=[
+                    # dcc.Tabs(parent_className='wrapper', className='filters', children=[
+                    #     dcc.Tab(label='Dashboard', className='', selected_className='', children=[
                             html.Div(className="grid grid--two-columns", children=[
                                 html.Div(id="data-chart", className="grid__all", children=[
                                     chart(data),
@@ -26,12 +26,12 @@ def layout(data, all_data):
                                 html.Div(id="word-cloud", className="grid__all", children=[
                                     wordcloud(data),
                                 ]),
-                                html.Div(id="top-funders", className="grid__1", children=[
-                                    top_funders(data),
-                                ]),
-                                html.Div(id="regions-chart", className="grid__1", children=[
-                                    regions(data),
-                                ]),
+                                # html.Div(id="top-funders", className="grid__1", children=[
+                                #     top_funders(data),
+                                # ]),
+                                # html.Div(id="regions-chart", className="grid__1", children=[
+                                #     regions(data),
+                                # ]),
                             ]),
                             html.Div(className="spacer-3"),
                             html.Div(className="grid grid--two-columns", children=[
@@ -73,8 +73,8 @@ def layout(data, all_data):
                                     '''),
                                 ]),
                             ]),
-                        ]),
-                        dcc.Tab(label='Data', className='', selected_className='', children=[
+                        # ]),
+                        # dcc.Tab(label='Data', className='', selected_className='', children=[
                             html.Div(className="grid grid--two-columns", children=[
                                 html.Div(className="grid__all", children=[
                                     html.Div([
@@ -94,6 +94,8 @@ def layout(data, all_data):
                                     ' ',
                                     html.A(className='button button--orange', href='/data/grants.json', target="_blank", children="Download all grants (JSON)"),
                                     ' ',
+                                    html.A(className='button button--orange', href='/data/la.csv', target="_blank", children="Download Local Authority summaries (CSV)"),
+                                    ' ',
                                     html.A(className='button button--orange', href="https://grantnav.threesixtygiving.org/search?json_query=%7B%22query%22%3A+%7B%22bool%22%3A+%7B%22must%22%3A+%7B%22query_string%22%3A+%7B%22query%22%3A+%22coronavirus+OR+pandemic+OR+covid+OR+%5C%22covid19%5C%22%22%2C+%22default_field%22%3A+%22%2A%22%7D%7D%2C+%22filter%22%3A+%5B%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%2C+%22must%22%3A+%7B%7D%2C+%22minimum_should_match%22%3A+1%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%7B%22range%22%3A+%7B%22amountAwarded%22%3A+%7B%7D%7D%7D%2C+%22must%22%3A+%7B%7D%2C+%22minimum_should_match%22%3A+1%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%7B%22range%22%3A+%7B%22awardDate%22%3A+%7B%22format%22%3A+%22year%22%2C+%22gte%22%3A+%222020%7C%7C%2Fy%22%2C+%22lte%22%3A+%222020%7C%7C%2Fy%22%7D%7D%7D%5D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%7D%7D%2C+%7B%22bool%22%3A+%7B%22should%22%3A+%5B%5D%7D%7D%5D%7D%7D%2C+%22sort%22%3A+%7B%22_score%22%3A+%7B%22order%22%3A+%22desc%22%7D%7D%2C+%22aggs%22%3A+%7B%22fundingOrganization%22%3A+%7B%22terms%22%3A+%7B%22field%22%3A+%22fundingOrganization.id_and_name%22%2C+%22size%22%3A+50%7D%7D%2C+%22recipientOrganization%22%3A+%7B%22terms%22%3A+%7B%22field%22%3A+%22recipientOrganization.id_and_name%22%2C+%22size%22%3A+3%7D%7D%2C+%22recipientRegionName%22%3A+%7B%22terms%22%3A+%7B%22field%22%3A+%22recipientRegionName%22%2C+%22size%22%3A+3%7D%7D%2C+%22recipientDistrictName%22%3A+%7B%22terms%22%3A+%7B%22field%22%3A+%22recipientDistrictName%22%2C+%22size%22%3A+3%7D%7D%2C+%22currency%22%3A+%7B%22terms%22%3A+%7B%22field%22%3A+%22currency%22%2C+%22size%22%3A+3%7D%7D%7D%2C+%22extra_context%22%3A+%7B%22awardYear_facet_size%22%3A+3%2C+%22amountAwardedFixed_facet_size%22%3A+3%7D%7D", target="_blank", children="Search on GrantNav"),
                                     html.Div(className="spacer-3"),
                                     html.P(
@@ -106,8 +108,8 @@ def layout(data, all_data):
                                     ),
                                 ]),
                             ]),
-                        ]),
-                    ]),
+                    #     ]),
+                    # ]),
                 ]),
             ]),
             # subscribe(),
